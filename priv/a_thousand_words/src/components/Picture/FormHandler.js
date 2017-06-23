05dd097a8f6077a0e32edf2a1b5430187b0d9d3e
Josh <joshmvince@gmail.com>
2017-06-23 17:14:17 +0100

    finish removal of old components

--- priv/a_thousand_words/src/components/Picture/FormHandler.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PictureForm from './Form.js';
-
-const PictureFormHandler = ({action, channel}) => {
-  return <div className="row"> <PictureForm action={action} channel={channel} /></div>
-}
-
-export default PictureFormHandler;
