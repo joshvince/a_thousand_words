6631506cbe1ad3fe41de4aaf417a2966315d7f5f
Josh <joshmvince@gmail.com>
2017-07-10 22:07:56 +0100

    maybe string to int wasmessing everything up

--- lib/a_thousand_words/web/endpoint.ex
+++ lib/a_thousand_words/web/endpoint.ex
@@ -54,6 +54,6 @@ defmodule AThousandWords.Web.Endpoint do
   """
   def load_from_system_env(config) do
     port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-    {:ok, Keyword.put(config, :http, [:inet6, port: String.to_integer(port)])}
+    {:ok, Keyword.put(config, :http, [:inet6, port: port])}
   end
 end
