f5faeb53220f174640badccf2b5a14ddcdec19f9
Josh <joshmvince@gmail.com>
2017-07-25 08:57:23 +0100

    remove react app

--- priv/a_thousand_words/src/encoders/Location/Decoder.test.js
+++
@@ -1,9 +0,0 @@
-import Decoder from './Decoder.js';
-
-test('converts objects to a string', () => {
-  let testInput = {
-    "coordinates": [1.2345, -9.8765],
-    "srid": null
-  }
-  expect(Decoder.decode(testInput)).toBe("1.2345,-9.8765")
-});
